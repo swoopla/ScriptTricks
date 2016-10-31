40815819ef53f4f7e8568efc5d1ef8bdb6003bc9
Pascal Vibet <pvibet@gmail.com>
2016-10-31 11:10:22 +0100

    * Update script

--- docker/maj_script_docker.sh
+++ docker/maj_script_docker.sh
@@ -1,55 +1,58 @@
 #!/bin/bash
 
-echo  ">>> Update docker-compose"
-URL=$(wget https://github.com/docker/compose/releases/ -qO- |grep "releases/download/[0-9].[0-9].[0-9]/docker-compose-Linux-x86_64" | head -1 |awk -F\" '{ print "https://github.com"$2}'
-wget ${URL} -O docker-compose
+WGET='wget -q'
+
+echo  "* Update docker-compose"
+URL=$(${WGET} https://github.com/docker/compose/releases/ -O- |grep "releases/download/[0-9].[0-9].[0-9]/docker-compose-Linux-x86_64" | head -1 |awk -F\" '{ print "https://github.com"$2}')
+${WGET} ${URL} -O docker-compose
 chmod +x docker-compose
-echo  ">>> To Do copy script to docker-* /usr/local/bin/"
+echo -e ">>> To Do copy script to docker-* /usr/local/bin/\n"
 
-echo  ">>> Update docker-compose bash_completion"
-wget https://raw.githubusercontent.com/docker/compose/$(./docker-compose version --short)/contrib/completion/bash/docker-compose -O docker-compose.bash
-echo  ">>> To Do copy *.bash to /etc/bash_completion.d/"
+echo  "* Update docker-compose bash_completion"
+${WGET} https://raw.githubusercontent.com/docker/compose/$(./docker-compose version --short)/contrib/completion/bash/docker-compose -O docker-compose.bash
+echo -e ">>> To Do copy *.bash to /etc/bash_completion.d/\n"
 
-echo  ">>> Update docker-machine"
-URL=$(wget https://github.com/docker/machine/releases/ -qO- |grep "releases/download/v[0-9].[0-9].[0-9]/docker-machine-Linux-x86_64" | head -1 |awk -F\" '{ print "https://github.com"$2}')
-wget ${URL} -O docker-machine
+echo  "* Update docker-machine"
+URL=$(${WGET} https://github.com/docker/machine/releases/ -O- |grep "releases/download/v[0-9].[0-9].[0-9]/docker-machine-Linux-x86_64" |head -1 |awk -F\" '{ print "https://github.com"$2}')
+${WGET} ${URL} -O docker-machine
 chmod +x docker-machine
-echo  ">>> To Do copy script to docker-* /usr/local/bin/"
+echo -e ">>> To Do copy script to docker-* /usr/local/bin/\n"
 
-echo  ">>> Update docker-machine bash_completion"
-wget https://github.com/docker/machine/blob/master/contrib/completion/bash/docker-machine.bash
-wget https://github.com/docker/machine/blob/master/contrib/completion/bash/docker-machine-wrapper.bash
-wget https://github.com/docker/machine/blob/master/contrib/completion/bash/docker-machine-prompt.bash
-echo  ">>> To Do copy *.bash to /etc/bash_completion.d/"
+echo  "* Update docker-machine bash_completion"
+${WGET} https://github.com/docker/machine/blob/master/contrib/completion/bash/docker-machine.bash
+${WGET} https://github.com/docker/machine/blob/master/contrib/completion/bash/docker-machine-wrapper.bash
+${WGET} https://github.com/docker/machine/blob/master/contrib/completion/bash/docker-machine-prompt.bash
+echo -e ">>> To Do copy *.bash to /etc/bash_completion.d/\n"
 
-echo  ">>> Update docker-volume-local-persist"
-URL=$(wget https://github.com/CWSpear/local-persist/releases -qO- |grep "releases/download/v[0-9].[0-9].[0-9]/local-persist-linux-amd64" | head -1 |awk -F\" '{ print "https://github.com"$2}')
-wget ${URL} -O docker-volume-local-persist
+echo  "* Update docker-volume-local-persist"
+URL=$(${WGET} https://github.com/CWSpear/local-persist/releases -O- |grep "releases/download/v[0-9].[0-9].[0-9]/local-persist-linux-amd64" | head -1 |awk -F\" '{ print "https://github.com"$2}')
+${WGET} ${URL} -O docker-volume-local-persist
 echo  ">>> To Do copy script to docker-* /usr/local/bin/"
-wget https://raw.githubusercontent.com/CWSpear/local-persist/master/init/systemd.service -qO- |sed -e 's#/bin/#/local/bin/#' > docker-volume-local-persist.service
+${WGET} https://raw.githubusercontent.com/CWSpear/local-persist/master/init/systemd.service -O- |sed -e 's#/bin/#/local/bin/#' > docker-volume-local-persist.service
 echo  ">>> To Do move *.service in /etc/systemd/system"
-echo  ">>> To Do: sudo systemctl daemon-reload && sudo systemctl enable docker-volume-local-persist && sudo systemctl start docker-volume-local-persist"
+echo -e ">>> To Do: sudo systemctl daemon-reload && sudo systemctl enable docker-volume-local-persist && sudo systemctl start docker-volume-local-persist\n"
 
-echo  ">>> Update drbdmanage-docker-volume"
+echo  "* Update drbdmanage-docker-volume"
 git clone http://git.drbd.org/drbdmanage-docker-volume.git/ && cd drbdmanage-docker-volume && \
 mv drbdmanage-docker-volume ../docker-volume-drbdmanage
 mv systemd/docker-drbdmanage-plugin.service ../docker-volume-drbdmanage.service
 mv systemd/docker-drbdmanage-plugin.socket ../docker-volume-drbdmanage.socket
+cd .. && rm -rf drbdmanage-docker-volume
 echo  ">>> To Do move *.service && *.socket in /etc/systemd/system"
-echo  ">>> To Do: sudo systemctl daemon-reload && sudo systemctl enable docker-volume-local-persist && sudo systemctl start docker-volume-local-persist"
+echo  ">>> To Do: sudo systemctl daemon-reload && sudo systemctl enable docker-volume-local-persist && sudo systemctl start docker-volume-local-persist\n"
 
-echo  ">>> Update docker-volume-vsphere"
-URL=$(wget https://github.com/vmware/docker-volume-vsphere/releases -qO- |grep "releases/download/[0-9].[0-9]/docker-volume-vsphere.*deb" |head -1 |awk -F\" '{ print "https://github.com"$2}')
-wget ${URL} -O docker-volume-vsphere.deb
-echo  ">>> To Do: sudo dpkg -i docker-volume-vsphere.deb"
+echo  "* Update docker-volume-vsphere"
+URL=$(${WGET} https://github.com/vmware/docker-volume-vsphere/releases -O- |grep "releases/download/[0-9].[0-9]/docker-volume-vsphere.*deb" |head -1 |awk -F\" '{ print "https://github.com"$2}')
+${WGET} ${URL} -O docker-volume-vsphere.deb
+echo -e ">>> To Do: sudo dpkg -i docker-volume-vsphere.deb\n"
 
-echo  ">>> Update docker-volume-convoy"
-URL=$(wget https://github.com/rancher/convoy/releases/ -qO- |grep "releases/download/v[0-9].[0-9].[0-9]/convoy.tar.gz |head -1 |awk -F\" '{ print "https://github.com"$2}'")
-wget ${URL} -O convoy.tar.gz
+echo  "* Update docker-volume-convoy"
+URL=$(${WGET} https://github.com/rancher/convoy/releases/ -O- |grep "releases/download/v[0-9].[0-9].[0-9]/convoy.tar.gz" |head -1 |awk -F\" '{ print "https://github.com"$2}')
+${WGET} ${URL} -O convoy.tar.gz
 tar xvf convoy.tar.gz
-mv convoy/{convoy,convoy-pdata_tools} .
-rm -rf convoy.tar.gz convoy/
+rename s/$/.tmp/ convoy
+mv convoy.tmp/{convoy,convoy-pdata_tools} .
+rm -r convoy.tar.gz convoy.tmp/
 echo  ">>> To Do copy script to docker-* /usr/local/bin/"
 echo  ">>> To Do mkdir -p /etc/docker/plugins/"
-echo  ">>> To Do bash -c 'echo "unix:///var/run/convoy/convoy.sock" > /etc/docker/plugins/convoy.spec'"
-
+echo -e ">>> To Do bash -c 'echo "unix:///var/run/convoy/convoy.sock" > /etc/docker/plugins/convoy.spec'\n"
